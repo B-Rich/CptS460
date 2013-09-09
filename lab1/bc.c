9595e66574909eea0ab73deb68dc51ce2c957e1d
goodboyrobot <afschuld@gmail.com>
2013-09-08 17:19:12 -0700

    works up to loading the booter into memory

--- lab1/bc.c
+++ lab1/bc.c
@@ -11,8 +11,8 @@ typedef struct ext2_inode         INODE;
 typedef struct ext2_dir_entry_2 DIR;
 
 #define BLK 1024
-char buf1[BLK], buf2[BLK], name[64];
-
+char buf1[BLK], buf2[BLK];
+char temp[256];
 int prints(char *s)
 {
      while(*s)
@@ -22,7 +22,7 @@ int scans(char *s)
 {
     int i = 0;
     char c = getc();
-    while(c!=0 && c!='\r')
+    while(c!='\r')
     {
         s[i]=c;
         i++;
@@ -36,8 +36,35 @@ u16 getblk(u16 blk, char *buf)
 {
         readfd( blk/18, ((blk*2)%36)/18, ((blk*2)%36)%18, buf);
 }
-char temp[256];
-main()
+u32 search(INODE * inodePtr,char * name)
+{
+    DIR * dp = (DIR*) buf2;
+    int j;
+    char *cp,tmp[64];
+        cp = buf2;
+        dp = (DIR*) buf2;
+        getblk((u16)inodePtr->i_block[0],buf2);
+        while (cp < buf2 +1024)
+        {
+            for (j = 0; j < dp->name_len; j++)
+            {
+                tmp[j] = (char)dp->name[j];
+            }
+            tmp[j] = 0;
+            if (strcmp(name,tmp) == 0)
+            {
+                return dp->inode;
+            }
+            else
+            {
+                cp += dp->rec_len;
+                dp = (DIR *)cp;
+            }    
+        }
+    return 0;
+}
+
+int main()
 { 
     u16    i,iblk;
     char   c;
@@ -45,7 +72,7 @@ main()
     INODE *ip;
     DIR   *dp;
 
-    prints("read decsriptor block #2 into buf1[]\n\r");
+    //prints("read decsriptor block #2 into buf1[]\n\r");
     getblk(2, buf1);
     gp = (GD *)buf1;
     iblk = (u16)gp->bg_inode_table;
@@ -54,9 +81,9 @@ main()
     getblk((u16)iblk, buf1);  // read first inode block block
     ip = (INODE *)buf1 + 1;   // ip->root inode #2
     
-    prints("read 0th data block of root inode into buf2[ ]\n\r");
+    //prints("read 0th data block of root inode into buf2[ ]\n\r");
 
-    getblk((u16)ip->i_block[0], buf2); 
+    /*getblk((u16)ip->i_block[0], buf2); 
     dp = (DIR *)buf2;           // buf2 contains DIR entries
 
     while((char *)dp < &buf2[BLK]){
@@ -65,15 +92,30 @@ main()
         temp[dp->name_len] = 0;
         prints(temp); putc(' ');
 
-        /*
+        
         c = dp->name[dp->name_len];
      v   dp->name[dp->name_len] = 0;
         prints(dp->name); putc(' ');
         dp->name[dp->name_len] = c;
-        */
+         
         dp = (DIR *)((char *)dp + dp->rec_len);
     }
+    */
 
     prints("\n\rEnter name of boot image: ");
-    scans(name);
+    scans(temp);
+    prints("\n\rfinding: ");
+    prints("boot");
+    getc();
+    i = search(ip,"boot");
+    prints("\n\rfinding: ");
+    prints(temp);
+    putc(i+'0');
+    getc();
+    ip = (INODE *)buf1 + i - 1;
+    i = search(ip,temp);
+    
+    ip = (INODE *)buf1 + i -1;
+    getblk((u16)ip->i_block[0],0x1000);
+    return 1;
 }
